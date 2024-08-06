1cd1d6d81643eb5077dfbff109a070e4852f7aec
Camilo Hernandez Romero <hrcamilo11@users.noreply.github.com>
2024-08-06 16:33:17 -0500

    Fixed: JS structure

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 import { fetchLatestVideo } from "./modules/fetchVideo.js";
 import { initModal } from "./modules/imageModal.js";
-import { openWp } from "./modules/openWhatsapp.js";
-import { translate } from "./modules/translate.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchLatestVideo().then(() => {});
-
-  openWp();
-
   initModal();
-
-  translate();
 });
