8a53597198be032d2498a13ae313f7f40839172c
Camilo Hernandez Romero <hrcamilo11@users.noreply.github.com>
2024-08-06 23:20:50 -0500

    Re-Build: Form - Embed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { fetchLatestVideo } from "./modules/fetchVideo.js";
 import { initModal } from "./modules/imageModal.js";
 
-
 document.addEventListener("DOMContentLoaded", () => {
   fetchLatestVideo().then(() => {});
   initModal();
+
 });
